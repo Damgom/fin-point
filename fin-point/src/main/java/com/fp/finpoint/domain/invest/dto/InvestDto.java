01b9760ca32b4de2d1270c61634ebfbee73d35cb
Damgom <wisewish1@gmail.com>
2023-06-19 14:59:28 +0900

    REFACTOR: remove some useless line

--- fin-point/src/main/java/com/fp/finpoint/domain/invest/dto/InvestDto.java
+++ fin-point/src/main/java/com/fp/finpoint/domain/invest/dto/InvestDto.java
@@ -9,24 +9,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class InvestDto {
-    private String subject;//제목
-    private String content;//내용
-//    public Long id;//글 번호
-//    public Member member;
-
-    // 조각정보
+    private String subject;
+    private String content;
     private String pieceName;
     private Long piecePrice;
     private Long pieceCount;
-
-//    Set<Member> liked;// 좋아요
-//    private Integer liked_cnt;// 좋아요 수
-
-    // 여기서 Invest는 return 타입
-//    public Invest toEntity() {
-//
-//        return new Invest(this.subject, this.content, this.id);
-//    }
     public Invest toEntity() {
         return new Invest(this.subject, this.content);
     }
